6630872e6c98f006c97168578c8278037e0425f9
bearsyker <bearsyker@gmail.com>
2024-03-27 04:07:33 -0300

    test commit

--- frontend/src/components/blogposts.jsx
+++ frontend/src/components/blogposts.jsx
@@ -3,11 +3,46 @@
 
 function BlogPost() {
     return (
+      
       <div>
         <div className="main-div">
           <div className="scnd-div">
             <div className="trd-div">
-
+              <article className="article">
+                            <div className="timedisplay">
+                                <time datetime="2023-03-16"
+                                      className="time">
+                                    Mar 16, 2023
+                                </time>
+                                <a
+                                    href="#"
+                                    className="">
+                                    {/*props.content.department*/}
+                                </a>
+                            </div>
+                            <div className="">
+                                <h3 className="">
+                                    <a href="#">
+                                        <span className=""></span>
+                                        {/*props.content.title*/}
+                                    </a>
+                                </h3>
+                                <p className="">
+                                    {/*props.content.info*/}
+                                </p>
+                            </div>
+                            <div className="">
+                                <div className="">
+                                    <p className="">
+                                        <a href="#">
+                                            <span className=""></span>
+                                            {/*props.content.author*/}
+                                        </a>
+                                    </p>
+                                    <p className="">{}</p>
+                                </div>
+                            </div>
+                </article>
             </div>
 
           </div>
