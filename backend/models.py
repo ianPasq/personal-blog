63fc3e66b003bd9bad5a63d457b2a344cdd32395
bearsyker <bearsyker@gmail.com>
2024-05-01 03:17:37 -0300

    fix "," =/

--- backend/models.py
+++ backend/models.py
@@ -13,7 +13,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     post_title = db.Column(db.String(100), nullable=False)
     post_content = db.Column(db.Text, nullable=False)
-    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE") nullable=False)
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
     comment = db.relationship('Comment', backref='user', passive_deletes=True)
     def to_json(self):
         return {
